d25896f40132908888e3620524193b57be965f44
karp57 <37594775+karp57@users.noreply.github.com>
2018-06-03 23:05:41 +0300

    Update Date.py

--- Date.py
+++ Date.py
@@ -1,8 +1,61 @@
-# Программа может работать только летом 2018 года
-# Запускайте из консоли
 import time
 import vk
 import vk_api
+
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+
+def pre_period(n):
+    f_counter = 0
+    while n % 5 == 0:
+        n //= 5
+        f_counter += 1
+    t_counter = 0
+    while n % 2 == 0:
+        n //= 2
+        t_counter += 1
+    return max(f_counter, t_counter)
+
+
+def dec_period(a, b, PrePeriod):
+    res = ""
+    end = False
+    counter = 0
+    while not end:
+        a %= b
+        a *= 10
+        if a == 0:
+            end = True
+        if counter == PrePeriod and not end:
+            old_a = a
+            res += "("
+        elif counter > PrePeriod:
+            if a == old_a:
+                res += ")"
+                end = True
+        counter += 1
+        if not end:
+            res += str(a // b)
+    return res
+
+def periodic_number(a, b):
+    a, b = a // gcd(a, b), b // gcd(a, b)
+    ans = str(a // b) + "."
+    a %= b
+    PrePeriod = pre_period(b)
+    return ans + dec_period(a, b, PrePeriod)
+
+def cur_time():
+    c_time = time.ctime()
+    c_time = c_time.split()
+    return c_time[3].split(':')
+
+
+def to_next_hour(c_time):
+    return (60 - int(c_time[1])) * 60 + 60 - int(c_time[2])
 
 
 def date():
@@ -19,20 +72,24 @@ def number_of_day(c_time):
     else:
         return int(c_time[1]) + 69 # В июле 31 свободный день
 
-
-def res_value(c_time):
-    return number_of_day(c_time) / number_of_day(['Aug', '31'])
-
+def change_name():
+    global u_log, u_pas
+    hours = (number_of_day(date()) - 1) * 24 + int(cur_time()[0])
+    vk = vk_api.VkApi(login = u_log, password = u_pas)
+    vk.auth()
+    vk.method('messages.editChat', {'chat_id':int(user_data[2]), 'title':periodic_number((9 * 2400 + hours), 2400)+ ' Д'})
+    return 0
 
 
 print('Для корректной работы в одной папке с этим файлом должен быть сохранён файл fin.txt.\nВ нём должны быть Ваш логин, пароль для VK и ваш chat_id беседы d20!\nОни должны быть записаны в одну строчку через пробел (сначала -- логин, потом -- пароль, в конце -- chat_id)!')
 fin = open('fin.txt', 'r')
 user_data = fin.readlines()[0].split()
 u_log, u_pas = user_data[0], user_data[1]
-vk = vk_api.VkApi(login = u_log, password = u_pas)
-vk.auth()
-print('Вы точно хотите поменять название беседы d20? (y/[n])?')
-if input() == 'y':
-    vk.method('messages.editChat', {'chat_id':int(user_data[2]), 'title':str(9 + res_value(date())) + ' Д'})
-    print('Ура, название успешно изменено!')
+change_name()
+print('Success')
+time.sleep(to_next_hour(cur_time()) + 10)
+while date() != ['Sep', '1']:
+    change_name()
+    print('Success')
+    time.sleep(3600)
 fin.close()
